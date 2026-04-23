d81780c9fbf057dc8fbd8eb707f47556d3cf95c8
buidat241007-tech <buidat241007@gmail.com>
2026-04-23 23:25:42 +0700

    Add files via upload

--- ontap-ss08-it202.sql
+++ ontap-ss08-it202.sql
@@ -20,21 +20,20 @@ create table Staffs(
 create table Rentals(
 	rental_id int primary key auto_increment not null,
     customer_id varchar(10) not null,
+	foreign key (customer_id) references Customers(customer_id),
     staff_id varchar(10) not null,
+	foreign key (staff_id) references Staffs(staff_id),
     rental_date timestamp not null,
     return_date timestamp not null,
-    status enum('Booked', 'PickedUp', 'Returned', 'Cancelled') not null,
-    foreign key (customer_id) references Customers(customer_id),
-	foreign key (staff_id) references Staffs(staff_id)
+    status enum('Booked', 'PickedUp', 'Returned', 'Cancelled') not null
 );
-
 create table Payments(
 	payment_id int primary key auto_increment not null,
     rental_id int not null,
+	foreign key (rental_id)references Rentals(rental_id),
     payment_method varchar(50) not null,
     payment_date timestamp default(current_timestamp()) not null,
-    amount decimal(15,2) check(amount >= 0 ),
-	foreign key (rental_id)references Rentals(rental_id)
+    amount decimal(15,2) check(amount >= 0 )
 );
 
 INSERT INTO Customers (customer_id, full_name, license_no, phone, address) VALUES
@@ -82,6 +81,17 @@ set performance_score = 4.8
 where staff_id = 'S002';
 select * from Staffs;
 
+ALTER TABLE Payments DROP FOREIGN KEY payments_ibfk_1;
+ALTER TABLE Payments MODIFY rental_id INT NULL;
+ALTER TABLE Payments
+ADD CONSTRAINT payments_fk
+FOREIGN KEY (rental_id) REFERENCES Rentals(rental_id)
+ON DELETE SET NULL;
+
+
+delete from Rentals
+where status = 'Cancelled' and rental_date < '2026-04-05 15:00:00';
+select * from Rentals;
 alter table Customers
 modify address varchar(200) not null default('Unknown');
 
@@ -91,15 +101,6 @@ add column email varchar(100) not null
 after full_name;
 select * from Staffs;
 
-alter table Payments
-drop foreign key rental_payments_ibfk_1;
-alter table Payments
-add constraint rental_payments_ibfk_1
-foreign key (rental_id)references Rentals(rental_id)
-on delete cascade;
-
-delete from Rentals
-where status = 'Cancelled' and rental_date < '2026-04-05 15:00:00';
 
 update Payments
 set amount = amount - (amount * 0.05)
@@ -146,5 +147,14 @@ SELECT payment_method, SUM(amount) AS total_revenue
 FROM Payments
 GROUP BY payment_method;
 
+SELECT s.staff_id, s.full_name, COUNT(r.rental_id) AS total_contracts
+FROM Rentals r
+INNER JOIN Staffs s ON r.staff_id = s.staff_id
+GROUP BY s.staff_id, s.full_name
+HAVING COUNT(r.rental_id) > 2;
+
+SELECT staff_id, full_name, salary_base
+FROM Staffs
+WHERE salary_base > (SELECT AVG(salary_base) FROM Staffs);
 
 
